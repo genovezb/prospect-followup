502a3213fdb7536570be4595d8a5169c1cfaf1c5
Bruno Genovez <genovezb@gmail.com>
2021-05-19 21:57:24 -0300

    added Filter Constructions by Responsable

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "constructions", to: "constructions#index"
   get "constructions/new", to: "constructions#new"
   get 'constructions/detail', to: 'constructions#detail', as: 'detail_constructions'
-  get 'constructions/id:/responsable', to: 'constructions#responsable', as: 'responsable_constructions'
+  # get 'constructions/id:/responsable', to: 'constructions#responsable', as: 'responsable_constructions'
   post "constructions", to: "constructions#create"
   get "constructions/:id", to: "constructions#show", as:'construction'
   get "constructions/:id/edit", to: "constructions#edit", as: 'edit_construction'
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get "responsables", to: "responsables#index"
   get "responsables/new", to: "responsables#new"
   post "responsables", to: "responsables#create"
+  get 'responsables/constructions', to: 'responsables#constructions', as: 'responsable_constructions'
   get "responsables/:id", to: "responsables#show", as: 'responsable'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
